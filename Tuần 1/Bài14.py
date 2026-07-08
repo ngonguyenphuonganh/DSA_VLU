6fd8a06dae7d31de3b217e3fb7b043332d33d22f
Phương Anh <ngonguyenphuonganh6@gmail.com>
2026-07-08 13:54:51 +0700

    Delete Tuần 1 directory

--- Tuần 1/Bài14.py
+++
@@ -1,9 +0,0 @@
-def tim_ten(ds, ten):
-    for i in range(len(ds)):
-        if ds[i].lower() == ten.lower():
-            return i
-    return -1
-ds = input("Nhập danh sách tên: ").split(",")
-ten = input("Nhập tên cần tìm: ")
-ket_qua = tim_ten(ds, ten)
-print("Vị trí:", ket_qua)
